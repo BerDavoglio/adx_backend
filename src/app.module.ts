8a74b3aa2573cf035c52d31aad04e16f326f3b49
berdavoglio <berdavoglio@gmail.com>
2023-04-23 00:22:11 -0300

    Finish simple backend for ADX/2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ProductsModule } from './products/products.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://berdavoglio:TnYrZhA2TgtvNcJa@cluster0.j3wl2iv.mongodb.net/test',
+      'mongodb+srv://berdavoglio:TnYrZhA2TgtvNcJa@cluster0.j3wl2iv.mongodb.net/?retryWrites=true&w=majority',
     ),
     ProductsModule,
   ],
